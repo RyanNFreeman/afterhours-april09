083ae1a875d5a5fd445baddbab3160449e1fda2f
Home <ryan.norman.freeman@gmail.com>
2019-04-10 17:04:29 -0600

    initial commit

---
+++ fun-times-with-jest2/fun-times-with-jest/src/App.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import App from "./App";
+import { render, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+it("renders without crashing", () => {});
